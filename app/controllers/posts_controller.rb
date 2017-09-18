99eadb66cb67477e09bc693578ea7a423ffaa623
donow365 <donow365@gmail.com>
2017-09-18 15:37:11 +0800

    implement post create function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,19 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user! , :only[:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create]
   def new
-    @group = Group.find(group[:id])
-    @post = Post
+    @group = Group.find(params[:group_id])
+    @post = Post.new
   end
 
 
   def create
-    @group = Group.find(group[:id])
+    @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
     @post.group = @group
 
     @post.user = current_user
     if @post.save
-      rediect_to group_path(group)
+      redirect_to group_path(@group)
     else
       render :new
     end
@@ -21,7 +21,8 @@ class PostsController < ApplicationController
 
   private
 
-  def
-
+  def post_params
+    params.require(:post).permit(:content)
+  end
 
 end
