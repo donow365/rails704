083c9bbc7a49daf8bc9fbcc9955ffcba3cd83b0b
donow365 <donow365@gmail.com>
2017-09-18 15:26:29 +0800

    add post button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user! , :only[:new, :create, :edit, :update, :destroy]
+  def new
+    @group = Group.find(group[:id])
+    @post = Post
+  end
+
+
+  def create
+    @group = Group.find(group[:id])
+    @post = Post.new(post_params)
+    @post.group = @group
+
+    @post.user = current_user
+    if @post.save
+      rediect_to group_path(group)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def
+
+
 end
